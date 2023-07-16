8ca65136ab55a942b2901267f99f54a3fd3f64ca
enuesaa <nsrciog@gmail.com>
2023-07-16 23:58:54 +0900

    feat: fetch rss feed by gofeed

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/mmcdole/gofeed"
+)
 
 func main() {
 	fmt.Println("aaa")
+
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://gigazine.net/news/rss_2.0/")
+	fmt.Println(feed.Title)
 }
