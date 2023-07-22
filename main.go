0f3a4290ddb945b9bedc36517c8c3d0657b00639
enuesaa <nsrciog@gmail.com>
2023-07-22 17:19:04 +0900

    feat: add sample code of fiber to implement proxy

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/mmcdole/gofeed"
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -12,4 +13,9 @@ func main() {
 	fp := gofeed.NewParser()
 	feed, _ := fp.ParseURL("https://gigazine.net/news/rss_2.0/")
 	fmt.Println(feed.Title)
+
+	app := fiber.New()
+	// app.Get("/api", handler.ListContents)
+	app.Static("/", "./public")
+	app.Listen(":3000")
 }
