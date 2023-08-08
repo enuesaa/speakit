e6b75a3b6ddc01172f629bd0ddbf0a1527bdc65a
enuesaa <nsrciog@gmail.com>
2023-08-09 03:46:39 +0900

    feat: implements fiber handler

---
+++ handler/feeds.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"fmt"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mmcdole/gofeed"
+)
+
+func ListFeeds(c *fiber.Ctx) error {
+	return c.JSON("")
+}
+
+func GetFeed(c *fiber.Ctx) error {
+	id := c.Params("id")
+	fmt.Printf("%s", id)
+
+	return c.JSON("")
+}
+
+type FeedRequest struct {
+    Name string `json:"name"`
+}
+func CreateFeed(c *fiber.Ctx) error {
+	body := new(FeedRequest)
+	if err := c.BodyParser(body); err != nil {
+		return err
+	}
+	fmt.Printf("%+v", body)
+
+	return c.JSON("")
+}
+
+func DeleteFeed(c *fiber.Ctx) error {
+	id := c.Params("id")
+	fmt.Printf("%s", id)
+
+	return c.JSON("")
+}
+
+func FetchFeed(c *fiber.Ctx) error {
+	fp := gofeed.NewParser()
+	feed, _ := fp.ParseURL("https://gigazine.net/news/rss_2.0/")
+	fmt.Println(feed.Title)
+	
+	return c.JSON("")
+}
--- main.go
+++ main.go
@@ -1,51 +1,44 @@
 package main
 
 import (
-	"fmt"
+	"strings"
 
+	"github.com/enuesaa/speakit/handler"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/proxy"
 	_ "github.com/mmcdole/gofeed"
 )
 
 func main() {
-	fmt.Println("aaa")
-
-	// fp := gofeed.NewParser()
-	// feed, _ := fp.ParseURL("https://gigazine.net/news/rss_2.0/")
-	// fmt.Println(feed.Title)
-
 	app := fiber.New()
-	// app.Get("/api", handler.ListContents)
-	app.Get("/*", func(c *fiber.Ctx) error {
+
+	app.Get("/feeds", handler.ListFeeds)
+	app.Get("/feeds/:id", handler.GetFeed)
+	app.Post("/feeds", handler.CreateFeed)
+	app.Delete("/feeds/:id", handler.DeleteFeed)
+
+	// - POST /jobs ... fetch rss feed and request to convert. 202 を返したい
+	// - GET /jobs
+	// - GET /jobs/{id}
+
+
+	// - GET /_admin
+	// - GET /_admin/feeds
+	// - GET /player
+	//   - start
+	//   - next
+	//   - prev
+	//   - stop
+	app.Get("/_admin/*", func(c *fiber.Ctx) error {
 		path := c.OriginalURL()
+		path = strings.TrimLeft(path, "/_admin")
 		return proxy.Forward("http://admin:3000" + path)(c)
 	})
+
+	// - GET /contents ... 一覧
+	// - GET /contents/{id} ... asset id を返す
+
+	// - GET /assets/{id}  ... wav file
+
 	app.Listen(":3000")
 }
-
-// Perform like API Gateway
-// URLのマッピングはGoのAppでいろいろ変えられるイメージ
-
-// ## URL体系
-// - GET /feeds ... feed list
-// - GET /feeds/{id}
-// - POST /feeds
-// - DELETE /feeds/{id}
-
-// - POST /jobs ... fetch rss feed and request to convert. 202 を返したい
-// - GET /jobs
-// - GET /jobs/{id}
-
-// - GET /contents ... 一覧
-// - GET /contents/{id} ... asset id を返す
-
-// - GET /assets/{id}  ... wav file
-
-// - GET /  ... admin
-// - GET /feeds
-// - GET /player
-//   - start
-//   - next
-//   - prev
-//   - stop
