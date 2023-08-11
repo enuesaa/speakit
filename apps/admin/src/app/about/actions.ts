69f537d8030ddb0f9ac8905fc8b043ac2b5efa1a
enuesaa <nsrciog@gmail.com>
2023-08-11 23:26:05 +0900

    chore: remove unused page and codes.

--- apps/admin/src/app/about/actions.ts
+++
@@ -1,9 +0,0 @@
-'use server'
-
-import { revalidatePath } from 'next/cache'
-
-export async function addNote(formData: FormData) {
-
-  console.log(formData)
-  revalidatePath('/about')
-}
--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -5,12 +5,7 @@ import Link from 'next/link'
 export default function Page() {
   const styles = {
     main: css({
-      fontSize: '2xl',
       fontWeight: 'bold',
-      color: 'violet.700',
-      _hover: {
-        color: 'red.300',
-      },
     })
   }
 
@@ -18,9 +13,9 @@ export default function Page() {
     <>
     <div className={container()}>
       <h1 className={styles.main}>
-        Hello 🐼!
+        speakit admin
       </h1>
-      <Link href='/about'>about</Link>
+      <Link href='/feeds'>Feeds</Link>
     </div>
     </>
   )
