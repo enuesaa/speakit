d93d3b6da34bff27c262416941d5c6e1649fec14
enuesaa <nsrciog@gmail.com>
2023-08-11 20:03:34 +0900

    feat: add redis repository (this is my code from teatime-app)

---
+++ repository/redis.go
@@ -0,0 +1,53 @@
+package repository
+
+import (
+	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"os"
+)
+
+type RedisRepositoryInterface interface {
+	Keys(pattern string) []string
+	Get(key string) string
+	Set(key string, value string)
+	Delete(key string)
+}
+
+type RedisRepository struct{}
+
+func (repo *RedisRepository) client() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDR"),
+		Password: "",
+		DB:       0,
+	})
+	return client
+}
+
+func (repo *RedisRepository) Keys(pattern string) []string {
+	vals, _ := repo.client().Keys(context.Background(), pattern).Result()
+	return vals
+}
+
+func (repo *RedisRepository) Get(key string) string {
+	val, err := repo.client().Get(context.Background(), key).Result()
+	if err != nil {
+		val = ""
+	}
+	return val
+}
+
+func (repo *RedisRepository) Set(key string, value string) {
+	err := repo.client().Set(context.Background(), key, value, 0).Err()
+	if err != nil {
+		fmt.Printf("%-v", err)
+	}
+}
+
+func (repo *RedisRepository) Delete(key string) {
+	err := repo.client().Del(context.Background(), key).Err()
+	if err != nil {
+		fmt.Printf("%-v", err)
+	}
+}
