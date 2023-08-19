1ef971e5f14181f865cb7580f594af3abfec53af
enuesaa <nsrciog@gmail.com>
2023-08-19 20:02:47 +0900

    feat: generate query fn from openapi

--- apps/admin/orval.config.ts
+++ apps/admin/orval.config.ts
@@ -4,7 +4,14 @@ export default defineConfig({
   speakitapi: {
     output: {
       client: 'react-query',
+      schemas: 'src/lib/schema',
       target: 'src/lib/api.ts',
+      override: {
+        mutator: {
+          path: './src/lib/client.ts',
+          name: 'useClient',
+        },
+      },
     },
     input: '../../openapi.yaml',
   },
---
+++ apps/admin/src/lib/client.ts
@@ -0,0 +1,21 @@
+// see https://github.com/anymaniax/orval/blob/master/samples/react-query/custom-client/src/api/mutator/custom-client.ts
+
+type Args = {
+  url: string
+  method: 'get'|'post'|'put'|'delete'
+  params?: Record<string, any>;
+  headers?: Record<string, any>;
+  data?: any;
+  signal?: any;
+}
+export const useClient = <T>() => {
+  return async ({ url, method, params, headers, data }: Args): Promise<T> => {
+    const res = await fetch(url + new URLSearchParams(params), {
+      method,
+      headers,
+      ...(data ? { body: JSON.stringify(data) } : {}),
+    })
+
+    return res.json()
+  }
+}
