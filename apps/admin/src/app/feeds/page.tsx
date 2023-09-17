7ee4bd7198e4d5c1c9762b2fca1c2ff32228b3bb
enuesaa <nsrciog@gmail.com>
2023-09-18 00:00:35 +0900

    feat: add fetch button

--- apps/admin/src/app/feeds/page.tsx
+++ apps/admin/src/app/feeds/page.tsx
@@ -1,7 +1,8 @@
 'use client'
 import { PageTitle } from '@/components/PageTitle'
-import { useGetapifeeds } from '@/lib/api'
+import { useGetapifeeds, usePostapifetchHook } from '@/lib/api'
 import { css } from '@/styled-system/css'
+import { MouseEventHandler } from 'react'
 
 export default function Page() {
   const { data, isLoading } = useGetapifeeds()
@@ -12,12 +13,30 @@ export default function Page() {
     })
   }
 
+
   return (
     <>
       <PageTitle title='Feeds' />
       {data?.items?.map((v,i) => (
-        <div key={i} className={styles.item}>{v.data?.name} {v.data?.url}</div>
+        <div key={i} className={styles.item}>
+          {v.data?.name}
+          {v.data?.url}
+          <FetchFeedButton id={v.id ?? ''}/>
+        </div>
       ))}
     </>
+  )
+}
+
+const FetchFeedButton = ({ id }: { id: string }) => {
+  const fetchFeeds = usePostapifetchHook()
+
+  const handleFeedFetch: MouseEventHandler<HTMLButtonElement> = (e) => {
+    e.preventDefault()
+    fetchFeeds({ id })
+  }
+
+  return (
+    <button onClick={handleFeedFetch}>fetch</button>
   )
 }
