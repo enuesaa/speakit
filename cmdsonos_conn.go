5861723e1da388c24d1710e34dd52a3ed8764838
nua <enuesaa@gmail.com>
2025-04-05 23:36:44 +0900

    fix: body も normalize できそう

--- cmdsonos_conn.go
+++ cmdsonos_conn.go
@@ -26,7 +26,6 @@ func (s *Sonos) getLocalIpAddr() (string, error) {
 		return "", err
 	}
 	defer conn.Close()
-
 	addr := conn.LocalAddr().(*net.UDPAddr)
 
 	return addr.IP.To4().String(), nil
@@ -37,13 +36,25 @@ func (s *Sonos) makeSubscribe(endpoint string) (*http.Request, error) {
 	return http.NewRequest("SUBSCRIBE", url, nil)
 }
 
+type Envelope struct {
+	XMLName       xml.Name `xml:"s:Envelope"`
+	XmlnsS        string   `xml:"xmlns:s,attr"`
+	EncodingStyle string   `xml:"s:encodingStyle,attr"`
+	Body          Body      `xml:"s:Body"`
+}
+
+type Body struct {
+	Action any `xml:",any"`
+}
+
 func (s *Sonos) makePost(endpoint string, body any) (*http.Request, error) {
 	url := fmt.Sprintf("http://%s:1400%s", s.ipAddr, endpoint)
-
 	envelope := Envelope{
 		XmlnsS:        "http://schemas.xmlsoap.org/soap/envelope/",
 		EncodingStyle: "http://schemas.xmlsoap.org/soap/encoding/",
-		Body:          body,
+		Body: Body{
+			Action: body,
+		},
 	}
 	envelopbytes, err := xml.Marshal(envelope)
 	if err != nil {
@@ -68,17 +79,6 @@ func (s *Sonos) SubscribeVolumeControl() (*http.Response, error) {
 	return s.clinet.Do(req)
 }
 
-type Envelope struct {
-	XMLName       xml.Name `xml:"s:Envelope"`
-	XmlnsS        string   `xml:"xmlns:s,attr"`
-	EncodingStyle string   `xml:"s:encodingStyle,attr"`
-	Body          any      `xml:"s:Body"`
-}
-
-type SetAVTransportURIBody struct {
-	SetAVTransportURI SetAVTransportURI `xml:"u:SetAVTransportURI"`
-}
-
 type SetAVTransportURI struct {
 	XMLName            xml.Name `xml:"u:SetAVTransportURI"`
 	XmlnsU             string   `xml:"xmlns:u,attr"`
@@ -88,13 +88,11 @@ type SetAVTransportURI struct {
 }
 
 func (s *Sonos) SetUriRequest() (*http.Response, error) {
-	body := SetAVTransportURIBody{
-		SetAVTransportURI: SetAVTransportURI{
-			XmlnsU:             "urn:schemas-upnp-org:service:AVTransport:1",
-			InstanceID:         0,
-			CurrentURI:         "https://www.ne.jp/asahi/music/myuu/wave/menuettm.mp3",
-			CurrentURIMetaData: "",
-		},
+	body := SetAVTransportURI{
+		XmlnsU:             "urn:schemas-upnp-org:service:AVTransport:1",
+		InstanceID:         0,
+		CurrentURI:         "https://www.ne.jp/asahi/music/myuu/wave/menuettm.mp3",
+		CurrentURIMetaData: "",
 	}
 	req, err := s.makePost("/MediaRenderer/AVTransport/Control", body)
 	if err != nil {
@@ -105,10 +103,6 @@ func (s *Sonos) SetUriRequest() (*http.Response, error) {
 	return s.clinet.Do(req)
 }
 
-type PlayBody struct {
-	Play Play `xml:"u:Play"`
-}
-
 type Play struct {
 	XMLName    xml.Name `xml:"u:Play"`
 	XmlnsU     string   `xml:"xmlns:u,attr"`
@@ -117,12 +111,10 @@ type Play struct {
 }
 
 func (s *Sonos) PlayRequest() (*http.Response, error) {
-	body := PlayBody{
-		Play: Play{
-			XmlnsU:     "urn:schemas-upnp-org:service:AVTransport:1",
-			InstanceID: 0,
-			Speed:      "1",
-		},
+	body := Play{
+		XmlnsU:     "urn:schemas-upnp-org:service:AVTransport:1",
+		InstanceID: 0,
+		Speed:      "1",
 	}
 	req, err := s.makePost("/MediaRenderer/AVTransport/Control", body)
 	if err != nil {
